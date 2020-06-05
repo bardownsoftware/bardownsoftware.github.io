28b00a2d6e68ba7803fce1c48811f72758feefb1
brian <brian@bardownsoftware.com>
2020-06-05 14:41:38 -0400

    Remove old code

--- code/Timer/Pods/Target Support Files/Pods-Timer/Pods-Timer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Timer : NSObject
-@end
-@implementation PodsDummy_Pods_Timer
-@end
--- code/Timer/Pods/Target Support Files/Pods-TimerTests/Pods-TimerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TimerTests : NSObject
-@end
-@implementation PodsDummy_Pods_TimerTests
-@end
